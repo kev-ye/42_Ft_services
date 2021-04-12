940cbe2b20591e40361b11e6ddb347b386e14d31
Kangkai Ye <yek@MacBook-Pro.local>
2021-04-12 22:51:35 +0200

    minikube installation fix

--- setup.sh
+++ setup.sh
@@ -6,139 +6,184 @@
 #    By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/05 09:58:40 by kaye              #+#    #+#              #
-#    Updated: 2021/04/12 15:03:55 by kaye             ###   ########.fr        #
+#    Updated: 2021/04/12 22:50:45 by kaye             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-## COLOR
+## ANSI COLOR CODES
 
-# BLACK "\033[1;30m"
-# RED "\033[1;31m"
-# GREEN "\033[1;32m"
-# YELLOW "\033[1;33m"
-# BLUE "\033[1;34m"
-# PURPLE "\033[1;35m"
-# CYAN "\033[1;36m"
+BLACK="\033[1;30m"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+NONE="\033[0m"
+CLR_SCREEN="\033[2J\033[H"$CYAN""
 
-echo " \033[2J\033[H\033[1;36m\
+# generate by the command "figlet"
+echo "$CLR_SCREEN$CYAN\
  _____ _                           _
 |  ___| |_     ___  ___ _ ____   _(_) ___ ___  ___
 | |_  | __|   / __|/ _ \ '__\ \ / / |/ __/ _ \/ __|
 |  _| | |_    \__ \  __/ |   \ V /| | (_|  __/\__ \\
 |_|    \__|___|___/\___|_|    \_/ |_|\___\___||___/
          |_____|
-\033[0m"
+$NONE"
 
-## CONFIG FOR LINUX
+## INSTALLATION OF MINIKUBE & KUBERNETES & DOCKER - FUNCTION
 
-if [ $(uname) = "Linux" ] ; then
-	echo "\033[1;31m\n(If you are in the VM, please check if you are running with 2 cores)\n\033[0m"
+function minikube_linux()
+{
+	echo ""$CYAN"🐧 OS : Linux\n"$NONE""
+
+	## CONFIG FOR LINUX
+	echo ""$RED"\n(If you are in the VM, please check if you are running with 2 cores)\n"$NONE""
 	cat /etc/group | grep "docker" | grep $(whoami) 2>/dev/null 1>&2
-	# $var -eq 0 : $var == 0 -> true
-	# $var -ne 0 : $var != 0 -> true
+	# -ne : !=
 	if [ $? -ne 0 ] ; then
 		# run docker without sudo
-		echo "\033[1;31m❗️Please do\033[0m \033[1;33m\"sudo usermod -aG docker $(whoami); newgrp docker\"\033[0m and \033[1;31mrerun\033[0m the script"
+		echo ""$RED"❗️Please do"$NONE" "$YELLOW"\"sudo usermod -aG docker $(whoami); newgrp docker\""$NONE" and "$RED"rerun"$NONE" the script"
 		exit
 	fi
-fi
 
-## MINIKUBE & KUBERNETES & DOCKER
-
-if [ $(uname) = "Linux" ] ; then
-	echo "\033[1;36m🐧 OS : Linux\n\033[0m"
 	# make sure docker is running
 	service docker restart
-	# run minikube
-	echo "\033[1;36m\n🛳 minikube running ...\033[0m"
-	minikube start --vm-driver=docker
-else
-	echo "\033[1;36m🍎 Os : Macos\n\033[0m"
+}
+
+function minikube_macos()
+{
+	echo ""$CYAN"🍎 Os : Macos\n"$NONE""
+
 	# installation of brew
 	if ! which brew 2>/dev/null 1>&2 ; then
-		echo "\033[1;32m\n🍺 brew installing ...\033[0m"
+		echo ""$GREEN"\n🍺 brew installing ..."$NONE""
 		rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
 	fi
-	# installation of kubectl
-	if ! which kubectl 2>/dev/null 1>&2 ; then
-		echo "\033[1;32m\n🛳  kubectl installing ...\033[0m"
-		brew install kubernetes-cli
-	fi
-	# installation of minikube
+
+	# installation of minikube (kubernetes-cli will installed during installation of minikube)
 	if ! which minikube 2>/dev/null 1>&2 ; then
-		echo "\033[1;32m\n🛳  minikube installing ...\033[0m"
+		echo ""$GREEN"\n🛳  minikube installing ..."$NONE""
 		brew install minikube
 	fi
+
+	# check if /goinfre folder exist (at 42)
 	if [ -d /goinfre ] ; then
-		# configuration in 42 campus
-		echo "\033[1;32m\n🐳 docker running ...\033[0m"
+
+		# configuration in 42
+		echo ""$GREEN"\n🐳 docker running ..."$NONE""
 		rm -rf /goinfre/$USER/docker
 		./srcs/init_docker.sh
+	
 		# link minikube folder to goinfre
 		if [ $MINIKUBE_HOME ] ; then
+
 			# clean old minikube
 			if [ "$MINIKUBE_HOME" = "/goinfre/$USER" ] ; then
-				echo "\033[1;36m\n♻️  clean old minikube if exist ...\033[0m"
+				echo ""$CYAN"\n♻️  clean old minikube if exist ..."$NONE""
 				minikube delete
 			fi
 		else
+	
 			# clean old minikube & minikube folder & relink to /goinfre/$USER
-			echo "\033[1;36m\n♻️  clean old minikube if exist ...\033[0m"
+			echo ""$CYAN"\n♻️  clean old minikube if exist ..."$NONE""
 			minikube delete
 			rm -rf $HOME/.kube
 			rm -rf $HOME/.minikube
-			echo "\033[1;32m\n↔️  link minikube folder to /goinfre/\$USER ...\033[0m"
-			echo "export MINIKUBE_HOME=/goinfre/$USER" >> $HOME/.zshrc
-			source $HOME/.zshrc
+			if ! grep MINIKUBE_HOME 2>/dev/null 1>&2 ; then
+				echo ""$GREEN"\n↔️  link minikube folder to /goinfre/\$USER ..."$NONE""
+				echo "export MINIKUBE_HOME=/goinfre/$USER" >> $HOME/.zshrc
+				source $HOME/.zshrc
+			fi
 		fi
 	else
-		echo "\033[1;32m\n🐳 docker running ...\033[0m"
+		echo ""$GREEN"\n🐳 docker running ..."$NONE""
 		open -a docker
+		echo ""$CYAN"\n♻️  clean old minikube if exist ..."$NONE""
+		minikube delete
 	fi
-	# run minikube
-	echo "\033[1;32m\n🛳  minikube running ...\033[0m"
-	minikube start --vm-driver=virtualbox --memory=2g --cpus=2 || echo "\033[1;31mTry the command \"minikube delete\" if failed and relaunch the script.\033[0m"
-fi
-
-## METALLB
-
-function install_metallb()
-{
-	## Preparation
-
-	# see what changes would be made, returns nonzero returncode if different
-	kubectl get configmap kube-proxy -n kube-system -o yaml | \
-	sed -e "s/strictARP: false/strictARP: true/" | \
-	kubectl diff -f - -n kube-system
-
-	# actually apply the changes, returns nonzero returncode on errors only
-	kubectl get configmap kube-proxy -n kube-system -o yaml | \
-	sed -e "s/strictARP: false/strictARP: true/" | \
-	kubectl apply -f - -n kube-system
-
-	## Installation By Manifest
-
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
-	# On first install only
-	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
-	kubectl apply -f srcs/yaml/metallb-configmap.yaml
 }
 
-if ! kubectl get pods -n metallb-system 2>/dev/null | grep "controller" | grep "Running" 2>/dev/null 1>&2 ; then
-	# enable metallb plugin with minikube addons command
-	echo "\033[1;32mEnabling & configuring metallb ...\033[0m"
-	if ! minikube addons enable metallb 2>/dev/null 1>&2 ; then
-		# install metallb in case of failure during activation
-		echo "\033[1;32mEnabling metallb with minikube addons command \033[1;31mfailed, try with manifest ...\033[0m"
-		install_metallb >/dev/null
-	else
-		kubectl apply -f srcs/yaml/metallb-configmap.yaml
+# SCRIPT
+
+if [ $# -lt 1 ] || [ $1 = 'run' ] || [ $1 = 'relaunch' ] ; then
+	if [ $(uname) = "Linux" ] ; then
+
+		# install minikube
+		minikube_linux
+
+		# run minikube
+		echo ""$CYAN"\n🛳 minikube running ..."$NONE""
+		minikube start --vm-driver=docker
+	elif [ $(uname) = "Darwin" ] ; then
+
+		# install minikube
+		minikube_macos
+
+		# run minikube
+		echo ""$GREEN"\n🛳  minikube running ..."$NONE""
+		minikube start --vm-driver=virtualbox --memory=2g --cpus=2 || echo ""$RED"Try the command \"minikube delete\" if failed and relaunch the script."$NONE""
+	fi
+
+	# enable dashboard plugin
+	echo ""$GREEN"Enabling dashboard ..."$NONE""
+	minikube addons enable dashboard
+
+	# tips
+	echo ""$YELLOW"\n✅ Lauch the command \"minikube delete\" to clean minikube when finished ✅\n"$NONE""
+
+	# reopen a new zsh because configuration of source ~/.zshrc isn't applicate on old zsh.
+	zsh
+
+elif [ $# -eq 1 ] && [ $1 = 'delete' ] ; then
+
+	# delete minikube
+	if which minikube 2>/dev/null 1>&2 ; then
+		echo ""$CYAN"\n♻️  clean minikube ..."$NONE""
+		minikube delete
+	fi
+
+elif [ $# -eq 1 ] && [ $1 = 'install' ] ; then
+	if [ $(uname) = "Linux" ] ; then
+
+		# install minikube
+		minikube_linux
+	elif [ $(uname) = "Darwin" ] ; then
+
+		# install minikube
+		minikube_macos
+	fi
+
+elif [ $# -eq 1 ] && [ $1 = 'clean_all' ] ; then
+	if [ $(uname) = "Linux" ] ; then
+
+		# delete minikube
+		if which minikube 2>/dev/null 1>&2 ; then
+			echo ""$CYAN"\n♻️  clean minikube ..."$NONE""
+			minikube delete
+		fi
+
+		# delete minikube folder
+		echo ""$CYAN"\n♻️  clean minikube folder ..."$NONE""
+		rm -rf $HOME/.kube
+		rm -rf $HOME/.minikube
+	elif [ $(uname) = "Darwin" ] ; then
+
+		# delete minikube
+		if which minikube 2>/dev/null 1>&2 ; then
+			echo ""$CYAN"\n♻️  clean minikube ..."$NONE""
+			minikube delete
+		fi
+
+		# delete minikube folder
+		echo ""$CYAN"\n♻️  clean minikube folder ..."$NONE""
+		rm -rf $HOME/.kube
+		rm -rf $HOME/.minikube
+
+		# delete minikube app
+		echo ""$CYAN"\n♻️  clean minikube app ..."$NONE""
+		brew uninstall minikube
+		brew uninstall kubernetes-cli
 	fi
 fi
-
-echo "\033[1;32mEnabling dashboard ...\033[0m"
-minikube addons enable dashboard
-echo "\033[1;33m\n✅ Lauch the command \"minikube delete\" to clean minikube when finished ✅\n\033[0m"
-zsh
