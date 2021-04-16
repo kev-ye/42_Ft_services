7dc00457ec5cfb1a75f27bc111429e5f770e7188
Kangkai Ye <yek@MacBook-Pro.local>
2021-04-16 21:12:29 +0200

    test container wp & mysql

--- srcs/config/mysql/debug.sh
+++ srcs/config/mysql/debug.sh
@@ -1,5 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    debug.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/04/16 19:09:37 by kaye              #+#    #+#              #
+#    Updated: 2021/04/16 19:33:19 by kaye             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 if [ $# -eq 1 ] && [ $1 = 'run' ] ; then
-	docker run -it --name sql sql
+	docker run -it -p 3306:3306 -e WP_PASS=wppass -e ADMIN_PASS=adminpass --name sql sql
 elif [ $# -eq 1 ] && [ $1 = 'build' ] ; then
 	docker build -t sql .
 elif [ $# -eq 1 ] && [ $1 = 'clean' ] ; then
--- srcs/config/mysql/srcs/init.sh
+++ srcs/config/mysql/srcs/init.sh
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    init.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/04/16 19:09:41 by kaye              #+#    #+#              #
+#    Updated: 2021/04/16 20:57:38 by kaye             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 # init
 openrc
 touch /run/openrc/softlevel
@@ -5,5 +17,23 @@ touch /run/openrc/softlevel
 # create mariadb
 /etc/init.d/mariadb setup
 
+# remote access
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+
 # start mariadb
-rc-service mariadb start
+rc-service mariadb restart
+
+# Configure wordpress database & admin database
+echo "create database wordpress;" | mysql -u root
+echo "create user 'wordpress'@'%' IDENTIFIED BY '$WP_PASS';" | mysql -u root
+echo "grant all privileges on wordpress.* to 'wordpress'@'%' with grant option;" | mysql -u root
+echo "create database admin;" | mysql -u root
+echo "create user 'admin'@'%' IDENTIFIED BY '$ADMIN_PASS';" | mysql -u root
+echo "grant all privileges on *.* to 'admin'@'%' with grant option;" | mysql -u root
+echo "flush privileges" | mysql -u root
+
+# Check database and grants #
+echo "show databases" | mysql -u root | grep 'wordpress'
+echo "show databases" | mysql -u root | grep 'admin'
+
+sh
