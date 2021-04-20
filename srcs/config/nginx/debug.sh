e6269d905571ccb6ac14f58b43dc684b8377a61c
Kangkai Ye <yek@MacBook-Pro.local>
2021-04-20 21:15:07 +0200

    nginx progressing

--- srcs/config/nginx/debug.sh
+++ srcs/config/nginx/debug.sh
@@ -6,13 +6,13 @@
 #    By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/16 19:09:15 by kaye              #+#    #+#              #
-#    Updated: 2021/04/16 19:09:16 by kaye             ###   ########.fr        #
+#    Updated: 2021/04/20 19:44:01 by kaye             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 if [ $# -eq 1 ] && [ $1 = 'run' ] ; then
 	# docker run -it -p 80:80 -p 443:443 -p 22:22 --name nginx nginx
-	docker run -it -p 80:80 -p 443:443 --name nginx nginx
+	docker run -it --network test -p 80:80 -p 443:443 --name nginx nginx
 elif [ $# -eq 1 ] && [ $1 = 'build' ] ; then
 	docker build -t nginx .
 elif [ $# -eq 1 ] && [ $1 = 'clean' ] ; then
--- srcs/config/nginx/srcs/init.sh
+++ srcs/config/nginx/srcs/init.sh
@@ -6,7 +6,7 @@
 #    By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/16 19:09:09 by kaye              #+#    #+#              #
-#    Updated: 2021/04/16 19:09:11 by kaye             ###   ########.fr        #
+#    Updated: 2021/04/20 21:14:09 by kaye             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,14 +18,17 @@
 openrc
 touch /run/openrc/softlevel
 
-# # run nginx
-# service nginx start
+# run nginx
+service nginx start
 
-# # run sshd
+# run sshd
 # service sshd restart 
 
-# # to keep the Container running
+# to keep the Container running
 # tail -f /dev/null
 
 # To run in foreground:
-nginx -g 'daemon off;'
+# nginx -g 'daemon off;'
+
+# debug
+sh
