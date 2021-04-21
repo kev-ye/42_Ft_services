577f3548f68b8388f0d2e2cfa6940f40b6888019
Kangkai YE <kaye@e2r4p18.42.fr>
2021-04-21 14:29:56 +0200

    nginx progressing

--- srcs/config/nginx/debug.sh
+++ srcs/config/nginx/debug.sh
@@ -6,11 +6,14 @@
 #    By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/16 19:09:15 by kaye              #+#    #+#              #
-#    Updated: 2021/04/20 19:44:01 by kaye             ###   ########.fr        #
+#    Updated: 2021/04/21 11:29:27 by kaye             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 if [ $# -eq 1 ] && [ $1 = 'run' ] ; then
+	if ! docker network ls | grep test ; then
+		docker network create test
+	fi
 	# docker run -it -p 80:80 -p 443:443 -p 22:22 --name nginx nginx
 	docker run -it --network test -p 80:80 -p 443:443 --name nginx nginx
 elif [ $# -eq 1 ] && [ $1 = 'build' ] ; then
