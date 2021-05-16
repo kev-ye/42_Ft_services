9a68d0979c5906628f2d44e905c9c47e28282196
Kangkai YE <kaye@e2r4p18.42.fr>
2021-05-16 14:19:58 +0200

    linux debug

--- srcs/config/grafana/srcs/init.sh
+++ srcs/config/grafana/srcs/init.sh
@@ -6,7 +6,7 @@
 #    By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/28 21:35:24 by kaye              #+#    #+#              #
-#    Updated: 2021/05/12 20:54:06 by kaye             ###   ########.fr        #
+#    Updated: 2021/05/16 14:17:21 by kaye             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,7 @@ touch /run/openrc/softlevel
 telegraf &
 
 # run grafana
-grafana-server --homepath=/usr/share/grafana
+cd /etc/grafana/bin/ && ./grafana-server
 
 # to keep the Container running
 # tail -f /dev/null
